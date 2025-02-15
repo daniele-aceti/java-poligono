a16a9c98b7e96681f511f757eb496e7223057819
daniele-aceti <daniele.aceti@hotmail.it>
2025-02-15 11:52:02 +0100

    fix: Poligono

--- src/App.java
+++ src/App.java
@@ -2,11 +2,11 @@
 public class App {
 
     public static void main(String[] args) {
-        Ottagono ottagono = new Ottagono(10.2);
+        Poligono ottagono = new Ottagono(10.2);
         System.out.println("L'area è " + ottagono.calcolaArea());
         System.out.println("Il perimetro è: " + ottagono.calcolaPerimetro());
 
-        Pentagono pentagono = new Pentagono(30);
+        Poligono pentagono = new Pentagono(30);
         System.out.println("L'area è " + pentagono.calcolaArea());
         System.out.println("Il perimetro è: " + pentagono.calcolaPerimetro());
     }
